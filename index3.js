206dee4c63efe92ef7044abf80e3e4a308bbeb95
neorselas <126072709+neorselas@users.noreply.github.com>
2023-04-09 23:34:57 +0800

    update

--- index3.js
+++ index3.js
@@ -1,18 +1,9 @@
-// function star_tree(){
-//     let star = " ";
-    
-//     for (let i = 0; i<=10; i++){
-//         document.getElementById('star_tree').innerHTML += ("<center>" + star + "<br>" + "</center>");
-//         star +="[  ]";
-//     }
-//     }
-    
-    // star_tree();
-    
+
+
 function rgb(){
-    var r = Math.floor((Math.random() * 255) + 1);
-    var g = Math.floor((Math.random() * 255) + 1);
-    var b = Math.floor((Math.random() * 255) + 1);
+    var r = Math.floor((Math.random() * 255));
+    var g = Math.floor((Math.random() * 255));
+    var b = Math.floor((Math.random() * 255));
            
     return "rgb(" + [r,g,b].join(',') + ")";
     }
